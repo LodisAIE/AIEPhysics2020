4b53c166adbe72e168b05f5817040fdd3c6e2f20
Lodis Perkins <lodisperkins44@gmail.com>
2020-09-01 16:45:16 -0500

    Added collision detection tutorial

--- Collision Detection/PhysicsObject.h
+++ Collision Detection/PhysicsObject.h
@@ -4,18 +4,18 @@ enum ShapeType
 {
 	PLANE,
 	SPHERE,
-	BOX
+	SHAPECOUNT
 };
 class PhysicsObject
 {
 protected:
 	ShapeType m_shapeID;
-	PhysicsObject();
+	PhysicsObject() {};
 	PhysicsObject(ShapeType a_shapeID) : m_shapeID(a_shapeID) {}
 
 public:
-
-	virtual void fixedUpdate(glm::vec2 gravity, float timeStep) = 0;
+	ShapeType GetShapeID() { return m_shapeID; }
+	virtual void fixedUpdate(glm::vec2 gravity, float timeStep) =0;
 	virtual void debug() = 0;
 	virtual void makeGizmo() = 0;
 	virtual void resetPosition() {};
